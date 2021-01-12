96bd8150cb4d96c3bd6ad593b950e2d290a8a34b
nat210b <nat_to_kung@hotmail.com>
2021-01-12 15:15:01 +0700

    frist commit

---
+++ addition.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+//ดึงLibrary input output
+void main(){
+//ฟังชั่นหลัก Fx main
+//------------ คำสั่งโปรแกรมบวกเลข --------------//
+    int num1, num2, total;
+    //ประกาศตัวแปร num1 num2 total 
+    printf("value of num 1 is : ");
+    scanf("%d" ,&num1);
+    printf("value of num 2 is : ");
+    scanf("%d" ,&num2);
+    total = num1 + num2;
+    printf("Result is : %d",total);
+
+//------------ สิ้นสุดโปรแกรมบวกเลข --------------//
+}
---
+++ helloworld.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+//preprocessing
+// ส่วนหัวสำหรับดึง libraly(1)
+
+//ส่วน function หลัก(2)
+int main()
+{
+    printf("NAME : Pakawat Prabthong\n"); 
+    printf("Nickname : Pooh\n");
+    printf("Age : 18 years\n");
+    printf("Favorite things : Sleep all day\n"); 
+    printf("What i want to do :Sleep Now!!! \n"); 
+    printf("My happiness is sleeping\n"); 
+    /* 
+        ส่วนคำสั่ง(3)
+        printf คือฟังชั่นที่ไว้สำหรับแสดงผลหน้าจอ
+    */
+
+    // วิธีการจัดชุดคำสั่งให้สวยงาม shift + alt + f
+    // ctrl + j
+}
