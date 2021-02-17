ca67ba4a79dfce256b9cb4f9e756a68769a7c402
nat210b <nat_to_kung@hotmail.com>
2021-02-17 19:05:16 +0700

    จัดการไฟล์

---
+++ array/array1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+    int num[3] = {7,14,21};
+    printf("%d\n",num[0]);
+    printf("%d\n",num[1]);
+    printf("%d\n",num[2]);
+
+    num[1] = 18;
+    printf("%d",num[1]);
+
+}
